8d366072aacba4727dcdecf22565f346ac59edfa
dragan <dragan@reversinglabs.com>
2020-07-25 11:31:47 +0200

    Refactor requirements, add circleci config

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from codecs import open
+import pkg_resources
 from setuptools import setup, find_packages
 
 import lintlens
@@ -8,17 +9,25 @@ import lintlens
 packages = find_packages(include=('lintlens*',), exclude=('*.tests',))
 
 
-requires = [
-    'six>=1.9.0',
-]
+with open('requirements/base.txt') as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
-tests_require = [
-    'pytest>=3.7.0',
-]
+
+with open('requirements/tests.txt') as requirements_txt:
+    tests_extras = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 
 with open('README.md', 'r', 'utf-8') as f:
     readme = f.read()
+
 
 setup(
     name='lintlens',
@@ -35,9 +44,9 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
     ),
-    install_requires=requires,
+    install_requires=install_requires,
     extras_require={
-        'tests': tests_require,
+        'tests': tests_extras,
     },
 
     entry_points={
