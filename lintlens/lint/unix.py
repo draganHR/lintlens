5ef02b9832d1d9080b96d81defc4dda41ca2ead5
dragan <dragan@reversinglabs.com>
2020-07-25 12:25:49 +0200

    Update lintlens/lint/unix.py

--- lintlens/lint/unix.py
+++ lintlens/lint/unix.py
@@ -5,24 +5,26 @@ from collections import namedtuple
 
 LintEntry = namedtuple('LintEntry', ['filename', 'line', 'column', 'message'])
 
+line_parts_pattern = re.compile(r'(?:\./)?(.+?):(\d+):(\d+): ?(.*)')
+
 
 def parse_lint_line(line):
     """Parse lint diff line
 
     >>> parse_lint_line('foo.txt:1:2: bar')
-    LintEntry(filename=u'foo.txt', start=1, count=2, content=u'bar')
+    LintEntry(filename='foo.txt', line=1, column=2, message='bar')
 
     >>> parse_lint_line('foo.txt:123:50: bar')
-    LintEntry(filename=u'foo.txt', start=123, count=50, content=u'bar')
+    LintEntry(filename='foo.txt', line=123, column=50, message='bar')
 
     >>> parse_lint_line('foo.txt:0:1:')
-    LintEntry(filename=u'foo.txt', start=0, count=1, content=u'')
+    LintEntry(filename='foo.txt', line=0, column=1, message='')
 
     >>> parse_lint_line('foo/foo bar.txt:0:1: baz')
-    LintEntry(filename=u'foo/foo bar.txt', start=0, count=1, content=u'baz')
+    LintEntry(filename='foo/foo bar.txt', line=0, column=1, message='baz')
     """
     # TODO: handle colon in filename
-    line_parts = re.match('(?:\./)?(.+?):(\d+):(\d+): ?(.*)', line)
+    line_parts = line_parts_pattern.match(line)
     lint_entry = LintEntry(
         line_parts.group(1),
         int(line_parts.group(2)),
